d849dec293678a2980b0cdb37ee7ff390f7bec50
Konstantin Gredeskoul <kigster@gmail.com>
2012-02-07 20:51:36 -0800

    Cleaning up AI header file and adding comments

--- Classes/AI/basic_ai.c
+++ Classes/AI/basic_ai.c
@@ -8,20 +8,87 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #include "basic_ai.h"
 
 
-int calc_score_at(int **work_board, int size, int next_player, int x, int y);
-int calc_score_one_way(int *row, int player);
+#define SEARCH_RADIUS 5
+#define NEED_TO_WIN 5
+
+
+#define RT_BREAK 1
+#define RT_CONTINUE 2
+
+//===============================================================================
+// REF: Go-Moku and Threat-Space Search, L.V. Allis, H.J. van den Herik (c) 1994
+//
+// winning position, straight five
+#define COST_FIVE 10000
+
+// the straight four is a a line of six squares, of which the attacker has 
+// occupied the four center squares, while the two outer squares are empty; 
+#define COST_STRAIGHT_FOUR 1000  
+
+// the three is either a line of seven squares of which the three center 
+// squares are occupied by the attacker, and the remaining four squares are 
+// empty, or a line of six squares, with three consecutive squares of the four
+// center squares occupied by the attacker, and the remaining three squares empty;
+#define COST_THREE 200
+
+// the four is defined as a line of five squares, of which the
+// attacker has occupied any four, with the fifth square empty;
+#define COST_FOUR 100
+
+// the broken three is a line of six squares of which the attacker has occupied 
+// three non-consecutive squares of the four center squares, while the other 
+// three squares are empty.
+#define COST_THREE_BROKEN 50
+
+// just two squares together...
+#define COST_TWO 10
+
+// useless piece of shit.
+#define COST_NOTHING 0
+
+//===============================================================================
+
+#define max(a,b) \
+({ __typeof__ (a) _a = (a); \
+__typeof__ (b) _b = (b); \
+_a > _b ? _a : _b; })
+
+#define min(a,b) \
+({ __typeof__ (a) _a = (a); \
+__typeof__ (b) _b = (b); \
+_a < _b ? _a : _b; })
+
+//===============================================================================
+// private functions
+int calc_score_at(int **work_board, 
+                  int size, 
+                  int next_player, 
+                  int x, 
+                  int y);
+
+int calc_score_in_one_dimension(int *row, 
+                                int player);
 
 int other_player(int player);
-int pick_next_random_move(int **board, int size, 
-                          int next_player, 
-                          int *move_x, int *move_y);
 
 int count_squares(int value, int player, int *last_square, 
                   int *hole_count, int *square_count, 
                   int *contiguous_square_count);
+
+int pick_next_random_move(int **board, 
+                          int size, 
+                          int next_player, 
+                          int *move_x, 
+                          int *move_y);
+
+
+//===============================================================================
+// implementation
 
 // 
 // Return 0 for success, -1 for error
@@ -85,81 +152,82 @@ int calc_score_at(int **board,
     int score = 0;
 
     // walk horizontally
-    for (i = 0; i < row_size; i++)      row[i] = -1;
+    memset(row, -1, row_size);
     for (i = x; i <= max_x; i++)        row[SEARCH_RADIUS + i - x] = board[i][y];
     for (i = x - 1; i >= min_x; i--)    row[i - min_x] = board[i][y];
 
-    score += calc_score_one_way(row, player);
+    score += calc_score_in_one_dimension(row, player);
     
     // walk vertically
-    for (i = 0; i < row_size; i++)      row[i] = -1;
+    memset(row, -1, row_size);
     for (i = y; i <= max_y; i++)        row[SEARCH_RADIUS + i - y] = board[x][i];
     for (i = y - 1; i >= min_y; i--)    row[i - min_y] = board[x][i];
 
-    score += calc_score_one_way(row, player);
+    score += calc_score_in_one_dimension(row, player);
 
-    int j;
-    
+    int j;    
     // walk diagonally top to bottom (left to right)
-    for (i = 0; i < row_size; i++)      row[i] = -1;
+    memset(row, -1, row_size);
     for (i = x, j = y; i <= max_x && j <= max_y; i++, j++)        
                                         row[SEARCH_RADIUS + i - x] = board[i][j];
     for (i = x - 1, j = y - 1; i >= min_x &&  j >= min_y; i--, j--)    
                                         row[i - min_x] = board[i][j];
     
-    score += calc_score_one_way(row, player);
+    score += calc_score_in_one_dimension(row, player);
 
+    
     // walk diagonally bottom to top (left to right)
-    for (i = 0; i < row_size; i++)      row[i] = -1;
+    memset(row, -1, row_size);
     for (i = x, j = y; i <= max_x && j >= min_y; i++, j--)        
                                         row[SEARCH_RADIUS + i - x] = board[i][j];
     for (i = x, j = y; i >= min_x && j <= max_y; i--, j++)    
                                         row[i - min_x] = board[i][j];
     
-    score += calc_score_one_way(row, player);
-    
+    score += calc_score_in_one_dimension(row, player);
     return score;
 }
 
-// calculates the score with a single dimensional array
-// where the cell of interest (being evaluated) is exactly in the
-// middle.
- 
-int calc_score_one_way(int *row, int player) {
+
+// calculates a total score for a configuration within a single-dimensional array
+// where the cell of interest (being evaluated) is exactly in the middle (and assumed 
+// to belong to player, and the array is (SEARCH_RADIUS * 2 + 1) in size.
+int calc_score_in_one_dimension(int *row, int player) {
     
-    int square_count = 1; // start by assuming we place there
-    int contiguous_square_count = 1; 
-    int right_hole_count = 0, left_hole_count = 0;   // holes found
+    int player_square_count = 1; // total squares found for this player in the strip
+    int player_contiguous_square_count = 1; // total contiguous squares found for this player
+    int right_hole_count = 0, left_hole_count = 0;   // holes found (we don't go past double-holes)
     int last_square;
     int i;
     
+    // walk from the middle till the end
     for (i = SEARCH_RADIUS + 1, last_square = player; i <= SEARCH_RADIUS*2 && row[i] >= 0; i++) {
         if (count_squares(row[i], player, &last_square, &right_hole_count, 
-                          &square_count, &contiguous_square_count) == RT_BREAK) 
+                          &player_square_count, &player_contiguous_square_count) == RT_BREAK) 
             break;
     }
+    // walk from the middle back to the beginning
     for (i = SEARCH_RADIUS - 1, last_square = player; i >= 0 && row[i] >= 0; i--) {
         if (count_squares(row[i], player, &last_square, &left_hole_count, 
-                          &square_count, &contiguous_square_count) == RT_BREAK) 
+                          &player_square_count, &player_contiguous_square_count) == RT_BREAK) 
             break;
     }
     
     int holes = left_hole_count + right_hole_count;
-    int total = holes + square_count;
+    int total = holes + player_square_count;
 
-    if (contiguous_square_count >= NEED_TO_WIN) {
+    if (player_contiguous_square_count >= NEED_TO_WIN) {
         return COST_FIVE;
-    } else if (contiguous_square_count == 4 && right_hole_count > 0 && left_hole_count > 0) {
+    } else if (player_contiguous_square_count == 4 && right_hole_count > 0 && left_hole_count > 0) {
         return COST_STRAIGHT_FOUR;
-    } else if (contiguous_square_count == 4 && (right_hole_count > 0 || left_hole_count > 0)) {
+    } else if (player_contiguous_square_count == 4 && (right_hole_count > 0 || left_hole_count > 0)) {
         return COST_FOUR;
-    } else if (contiguous_square_count == 3 && (right_hole_count > 0 && left_hole_count > 0)) {
+    } else if (player_contiguous_square_count == 3 && (right_hole_count > 0 && left_hole_count > 0)) {
         return COST_THREE;
-    } else if (square_count >= 3 &&  (right_hole_count > 0 || left_hole_count > 0) && total >= 5) {
+    } else if (player_square_count >= 3 &&  (right_hole_count > 0 || left_hole_count > 0) && total >= 5) {
         return COST_THREE_BROKEN;
-    } else if (contiguous_square_count >= 2 &&  (right_hole_count > 0 || left_hole_count > 0) && total >= 4) { 
+    } else if (player_contiguous_square_count >= 2 &&  (right_hole_count > 0 || left_hole_count > 0) && total >= 4) { 
         return COST_TWO;
-    } else if (square_count >= 2 &&  (right_hole_count > 0 || left_hole_count > 0)) { 
+    } else if (player_square_count >= 2 &&  (right_hole_count > 0 || left_hole_count > 0)) { 
         return COST_TWO - 1;
     }
     return COST_NOTHING;
--- Classes/AI/basic_ai.h
+++ Classes/AI/basic_ai.h
@@ -1,6 +1,6 @@
 //
 //  basic_ai.h
-//  gomoku
+//  gomoku ANSI C AI basic interface
 //
 //  Created by Konstantin Gredeskoul on 2/3/12.
 //  Copyright (c) 2012 LeftCtrl Labs. All rights reserved.
@@ -9,62 +9,21 @@
 #ifndef gomoku_basic_ai_h
 #define gomoku_basic_ai_h
 
+#define RT_SUCCESS 0
+#define RT_FAILURE -1
 #define EMPTY 0
 
-#define RT_SUCCESS 0
-#define RT_BREAK 1
-#define RT_CONTINUE 2
-#define RT_FAILURE -1
-
-#define SEARCH_RADIUS 5
-#define NEED_TO_WIN 5
-
-//===============================================================================
-
-// REF: Go-Moku and Threat-Space Search, L.V. Allis, H.J. van den Herik (c) 1994
-//
-// winning position, straight five
-#define COST_FIVE 10000
-// the straight four is a a line of six squares, of which the attacker has 
-// occupied the four center squares, while the two outer squares are empty; 
-#define COST_STRAIGHT_FOUR 1000  
-// the four is defined as a line of five squares, of which the
-// attacker has occupied any four, with the fifth square empty;
-#define COST_FOUR 100
-// the three is either a line of seven squares of which the three center 
-// squares are occupied by the attacker, and the remaining four squares are 
-// empty, or a line of six squares, with three consecutive squares of the four
-// center squares occupied by the attacker, and the remaining three squares empty;
-#define COST_THREE 200
-// the broken three is a line of six squares of which the attacker has occupied 
-// three non-consecutive squares of the four center squares, while the other 
-// three squares are empty.
-#define COST_THREE_BROKEN 50
-#define COST_TWO 10
-// useless
-#define COST_NOTHING 0
-
-
-
-// states
-#define STATE_OPEN
-
-//===============================================================================
-
-#define max(a,b) \
-({ __typeof__ (a) _a = (a); \
-__typeof__ (b) _b = (b); \
-_a > _b ? _a : _b; })
-
-#define min(a,b) \
-({ __typeof__ (a) _a = (a); \
-__typeof__ (b) _b = (b); \
-_a < _b ? _a : _b; })
-
+/**
+ * For board of a given size, assume the board is filled with 0 (empty), 1 (X) or 2 (0).
+ * next_player is either 0 or 1.
+ *
+ * AI calculates the next move, and saves it into &move_x, &move_y, and returns RC_SUCCESS
+ * or RT_FAILURE if no appropriate next move is found.
+ *
+ */
 int pick_next_move(int **board, int size, 
                     int next_player, 
                     int *move_x, int *move_y);
-
 
 
 #endif
