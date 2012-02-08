19be376687d5c215c7caa90bc146871769abfb02
Konstantin Gredeskoul <kigster@gmail.com>
2012-02-07 20:54:43 -0800

    Last reformat!

--- Classes/AI/basic_ai.h
+++ Classes/AI/basic_ai.h
@@ -11,22 +11,25 @@
 
 #define RT_SUCCESS 0
 #define RT_FAILURE -1
+
 #define EMPTY 0
 
 /**
- * For board of a given size, assume the board is filled with 0 (empty), 1 (X) or 2 (0).
+ * For a board of a given size, assume that it is filled with int 0 (empty), 1 (X) or 2 (0).
  * next_player is set to either 1 or 2.
  *
- * AI calculates the next move, and saves it into &move_x, &move_y, and returns RC_SUCCESS;
+ * AI calculates the next move, and saves it into &move_x, &move_y, and returns RT_SUCCESS;
  * or returns RT_FAILURE if no appropriate next move is found.
  *
  * NOTE: future revisions should allow passing flags to control the gameplay, such as Renji
  * variations.
  *
  */
-int pick_next_move(int **board, int size, 
-                    int next_player, 
-                    int *move_x, int *move_y);
+int pick_next_move(int **board, 
+                   int size, 
+                   int next_player, 
+                   int *move_x, 
+                   int *move_y);
 
 
 #endif
